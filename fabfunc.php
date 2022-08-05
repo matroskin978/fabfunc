dbfed9470786334a7386a6d5de85ed3be56b6fff
Andrey Kudlay <matroskin8@i.ua>
2022-08-05 11:55:40 +0300

    first commit

---
+++ fabfunc.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: Fabulous Functionality
+Plugin URI: https://webformyself.com
+Description: Краткое описание плагина Fabulous Functionality.
+Version: 1.0
+Author: Andrey Kudlay
+Author URI: https://webformyself.com
+*/
+
